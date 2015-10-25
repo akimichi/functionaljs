d05ae5f67e083fb9427bea746d29e528178d893a
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2015-10-26 07:29:53 +0900

    update

--- test/interpreter.spec.js
+++ test/interpreter.spec.js
@@ -26,16 +26,20 @@ describe('λ計算機', () => {
     return exp.call(pattern, pattern);
   };
   var number = (value) => {
+	expect(value).to.a('number');
     return (pattern) => {
       return pattern.number(value);
     };
   };
   var variable = (name) => {
+	expect(name).to.a('string');
     return (pattern) => {
-      return pattern.variable(value);
+      return pattern.variable(name);
     };
   };
   var lambda = (arg, body) => {
+	expect(arg).to.a('function');
+	expect(body).to.a('function');
     return (pattern) => {
       return pattern.lambda(arg, body);
     };
@@ -45,11 +49,11 @@ describe('λ計算機', () => {
       return pattern.application(name,arg);
     };
   };
-  var closure = (id, body, env) => {
-    return (arg) => {
-      return evaluate(body, extendEnv(id, body,env));
-    };
-  };
+  // var closure = (id, body, env) => {
+  //   return (arg) => {
+  //     return evaluate(body, extendEnv(id, body,env));
+  //   };
+  // };
   // ## 式の評価関数
   var evaluate = (exp, env) => {
 	return match(exp,{
@@ -65,8 +69,8 @@ describe('λ計算機', () => {
 		};
 		// return closure(arg, body, env);
 	  },
-	  application: (name, arg) => {
-        var rator = evaluate(name, env)
+	  application: (func, arg) => {
+        var rator = evaluate(func, env)
         var rand = evaluate(arg, env)
         return rator(rand);
       }
@@ -97,5 +101,131 @@ describe('λ計算機', () => {
     //   (expect(intp.applyEnv(newEnv, "z"))).to.be(3);
     //   return (expect(intp.applyEnv(newEnv, "y"))).to.be(2);
     // });
+  });
+  describe('eval', () => {
+    it('evaluate number', (next) => {
+      expect(
+		evaluate(number(2), emptyEnv)
+	  ).to.be(
+		2
+	  );
+	  next();
+    });
+    it('evaluate variable', (next) => {
+      var env = extendEnv("x",1, emptyEnv);
+      expect(
+		evaluate(variable("x"), env)
+	  ).to.be(
+		1
+	  );
+      expect(
+		evaluate(variable("y"), env)
+	  ).to.be(
+		undefined
+	  );
+	  next();
+    });
+    it('evaluate a constant function application with eval', (next) => {
+      var constant = lambda(variable("x"),number(1))
+      expect(
+		evaluate(constant, emptyEnv)
+	  ).to.a(
+		'function'
+	  );
+	  next();
+    });
+  //   it('evaluate id function as an object with eval', function() {
+  //     var func, id;
+  //     id = {
+  //       arg: "x",
+  //       body: "x"
+  //     };
+  //     func = intp["eval"](id, intp.emptyEnv());
+  //     (expect(fj.isFunction(func))).to.be(true);
+  //     (expect(func(1))).to.be(1);
+  //     return (expect(intp["eval"]([id, 1], intp.emptyEnv()))).to.be(1);
+  //   });
+  //   return it('evaluate logical function application with eval', function() {
+  //     var andFun, falseFun, ifFun, notFun, orFun, trueFun;
+  //     trueFun = {
+  //       arg: "x",
+  //       body: {
+  //         arg: "y",
+  //         body: "x"
+  //       }
+  //     };
+  //     falseFun = {
+  //       arg: "x",
+  //       body: {
+  //         arg: "y",
+  //         body: "y"
+  //       }
+  //     };
+  //     notFun = {
+  //       arg: "x",
+  //       body: [["x", falseFun], trueFun]
+  //     };
+  //     andFun = {
+  //       arg: "x",
+  //       body: {
+  //         arg: "y",
+  //         body: [["x", "y"], falseFun]
+  //       }
+  //     };
+  //     orFun = {
+  //       arg: "x",
+  //       body: {
+  //         arg: "y",
+  //         body: [["x", trueFun], "y"]
+  //       }
+  //     };
+  //     ifFun = {
+  //       arg: "pred",
+  //       body: {
+  //         arg: "x",
+  //         body: {
+  //           arg: "y",
+  //           body: [["pred", "x"], "y"]
+  //         }
+  //       }
+  //     };
+  //     (expect(intp["eval"]([[trueFun, 1], 0], intp.emptyEnv()))).to.be(1);
+  //     (expect(intp["eval"]([[[notFun, trueFun], 1], 0], intp.emptyEnv()))).to.be(0);
+  //     (expect(intp["eval"]([[[[andFun, trueFun], trueFun], 1], 0], intp.emptyEnv()))).to.be(1);
+  //     return (expect(intp["eval"]([[[[[ifFun, trueFun], falseFun], trueFun], 1], 0], intp.emptyEnv()))).to.be(0);
+  //   });
+  // });
+  // return describe('consBinding', function() {
+  //   it('constructs new binding with consBinding', function() {
+  //     var emptyBinding, firstBinding, secondBinding;
+  //     emptyBinding = {};
+  //     firstBinding = intp.consBinding("a", 1)(emptyBinding);
+  //     (expect(firstBinding['a'])).to.be(1);
+  //     secondBinding = intp.consBinding("b", 2)(firstBinding);
+  //     (expect(secondBinding['b'])).to.be(2);
+  //     return (expect(secondBinding['a'])).to.be(1);
+  //   });
+  //   it('can be composed', function() {
+  //     var composedBinding, emptyBinding, firstBinding, secondBinding, thirdBinding;
+  //     firstBinding = intp.consBinding("a", 1);
+  //     secondBinding = intp.consBinding("b", 2);
+  //     thirdBinding = intp.consBinding("c", 3);
+  //     emptyBinding = {};
+  //     composedBinding = fj.compose(fj.compose(firstBinding, secondBinding), thirdBinding)(emptyBinding);
+  //     (expect(composedBinding['a'])).to.be(1);
+  //     (expect(composedBinding['b'])).to.be(2);
+  //     return (expect(composedBinding['c'])).to.be(3);
+  //   });
+  //   return it('pipes consBinding with pipe', function() {
+  //     var combinedBinding, emptyBinding, firstBinding, secondBinding, thirdBinding;
+  //     firstBinding = intp.consBinding("a", 1);
+  //     secondBinding = intp.consBinding("b", 2);
+  //     thirdBinding = intp.consBinding("c", 3);
+  //     emptyBinding = {};
+  //     combinedBinding = fj.pipe([firstBinding, secondBinding, thirdBinding])(emptyBinding);
+  //     (expect(combinedBinding['a'])).to.be(1);
+  //     (expect(combinedBinding['b'])).to.be(2);
+  //     return (expect(combinedBinding['c'])).to.be(3);
+  //   });
   });
 });

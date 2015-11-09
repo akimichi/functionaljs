59e4f962f24f21f0e0a480980b327cd008add36b
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2015-11-09 19:50:45 +0900

    untabify

--- test/chap06.spec.js
+++ test/chap06.spec.js
@@ -9,7 +9,7 @@ describe('関数の基本', () => {
     match: (data, pattern) => {
       return data(pattern);
     },
-	empty: (pattern) => {
+    empty: (pattern) => {
       return pattern.empty;
     },
     cons: (value, list) => {
@@ -43,34 +43,34 @@ describe('関数の基本', () => {
     }
   };
   var infiniteLoop = (_) => {
-	return infiniteLoop(_);
+    return infiniteLoop(_);
   };
   describe('関数の定義', () => {
-	it('関数の変数へのバインド', (next) => {
+    it('関数の変数へのバインド', (next) => {
       /* #@range_begin(function_bound_to_variable) */
       var id = (any) => {
-		return any;
+        return any;
       };
       /* #@range_end(function_bound_to_variable) */
       /* #@range_begin(identity_function_test) */
       expect(
-		id(1)
+        id(1)
       ).to.eql(
-		1
+        1
       );
       expect(
-		id("a")
+        id("a")
       ).to.eql(
-		"a"
+        "a"
       );
       /* #@range_end(identity_function_test) */
       next();
-	});
-	it('関数とリストの類似性', (next) => {
+    });
+    it('関数とリストの類似性', (next) => {
       var match = (data, pattern) => {
         return data(pattern);
       };
-	  var empty = (pattern) => {
+      var empty = (pattern) => {
         return pattern.empty;
       };
       var cons = (value, list) => {
@@ -102,121 +102,121 @@ describe('関数の基本', () => {
           },
         });
       };
-	  /*
-		~~~haskell
-		list2fct :: Eq a => [(a,b)] -> a -> b
-		list2fct [] _ = error "function not total"
-		list2fct ((u,v):uvs) x | x == u = v
+      /*
+        ~~~haskell
+        list2fct :: Eq a => [(a,b)] -> a -> b
+        list2fct [] _ = error "function not total"
+        list2fct ((u,v):uvs) x | x == u = v
                                | otherwise = list2fct uvs x
-		fct2list :: (a -> b) -> [a] -> [(a,b)]
-		fct2list f xs = [ (x, f x) | x <- xs ]
-		~~~
-	  */
-	  // var list2function = (list) => {
-	  // 	return (any) => {
-	  // 	  if(head(list)) {
-	  // 		if(head(list) === any){
-	  // 		  return
-	  // 		} else {
-	  // 		}
-	  // 	} else {
-	  // 	}
-	  // };
+        fct2list :: (a -> b) -> [a] -> [(a,b)]
+        fct2list f xs = [ (x, f x) | x <- xs ]
+        ~~~
+      */
+      // var list2function = (list) => {
+      //    return (any) => {
+      //      if(head(list)) {
+      //        if(head(list) === any){
+      //          return
+      //        } else {
+      //        }
+      //    } else {
+      //    }
+      // };
       next();
-	});
+    });
   });
   describe('関数の評価戦略', () => {
-	/* #@range_begin(strict_evaluation_in_javascript) */
-	var left = (x,y) => {
-	  return x;
-	};
-	expect(
-	  left(1, 2)
-	).to.eql(
-	  1
-	);
+    /* #@range_begin(strict_evaluation_in_javascript) */
+    var left = (x,y) => {
+      return x;
+    };
+    expect(
+      left(1, 2)
+    ).to.eql(
+      1
+    );
     var infiniteLoop = (_) => {
       return infiniteLoop(_);
     };
-	
+    
     /* このテストは実行されると無限ループになるのでコメントアウトしています
-	expect(
-	  left(1, infiniteLoop())
-	).to.be.ok()
-	*/
-	/* #@range_end(strict_evaluation_in_javascript) */
-	
+    expect(
+      left(1, infiniteLoop())
+    ).to.be.ok()
+    */
+    /* #@range_end(strict_evaluation_in_javascript) */
+    
   });
   describe('関数を合成する', () => {
-	it('関数を連続して適用する', (next) => {
-	  /* #@range_begin(function_applied_sequentially) */
-	  var double = (n) => {
-		return n * 2;
-	  };
-	  var negate = (n) => {
-		return - n;
-	  };
-	  expect(
-		negate(double(2))
-	  ).to.eql(
-		-4
-	  )
-	  /* #@range_end(function_applied_sequentially) */
-	  /* #@range_begin(function_applied_twice) */
-	  var twice = (f) => {
-		return (n) => {
-		  return f(f(n));
-		};
-	  };
-	  var succ = (x) => {
-		return x + 1;
-	  };
-	  expect(
-		twice(succ)(2)
-	  ).to.eql(
-		4
-	  );
-	  /* #@range_end(function_applied_twice) */
-	  /* #@range_begin(function_applied_ntimes) */
-	  var applyNtimes = (n) => {
-		return (func) => {
-		  return (init) => {
-			return (accumulator) => {
-			  if(n === 0) {
-				return accumulator;
-			  } else {
-				return applyNtimes(n - 1)(func)(init)(func(accumulator))
-			  };
-			};
-		  };
-		};
-	  };
-	  expect(
-		applyNtimes(4)(succ)(0)(0) // succ(succ(succ(succ(0))))
-	  ).to.eql(
-		4
-	  );
-	  /* #@range_end(function_applied_ntimes) */
+    it('関数を連続して適用する', (next) => {
+      /* #@range_begin(function_applied_sequentially) */
+      var double = (n) => {
+        return n * 2;
+      };
+      var negate = (n) => {
+        return - n;
+      };
+      expect(
+        negate(double(2))
+      ).to.eql(
+        -4
+      )
+      /* #@range_end(function_applied_sequentially) */
+      /* #@range_begin(function_applied_twice) */
+      var twice = (f) => {
+        return (n) => {
+          return f(f(n));
+        };
+      };
+      var succ = (x) => {
+        return x + 1;
+      };
+      expect(
+        twice(succ)(2)
+      ).to.eql(
+        4
+      );
+      /* #@range_end(function_applied_twice) */
+      /* #@range_begin(function_applied_ntimes) */
+      var applyNtimes = (n) => {
+        return (func) => {
+          return (init) => {
+            return (accumulator) => {
+              if(n === 0) {
+                return accumulator;
+              } else {
+                return applyNtimes(n - 1)(func)(init)(func(accumulator))
+              };
+            };
+          };
+        };
+      };
+      expect(
+        applyNtimes(4)(succ)(0)(0) // succ(succ(succ(succ(0))))
+      ).to.eql(
+        4
+      );
+      /* #@range_end(function_applied_ntimes) */
       next();
-	});
-	describe('関数合成', () => {
-	  /* #@range_begin(compose_definition) */
-	  var compose = (f) => {
-		return (g) => {
-		  return (arg) =>{
-			return f(g(arg));
-		  };
-  		};
-	  };
-	  // var compose = (f) => {
-	  // 	var self = this;
-	  // 	return (g) => {
-	  // 	  return (_) => {
-	  // 		return f.call(self, g.apply(self, arguments));
-	  // 	  };
-  	  // 	};
-	  // };
-	  /* #@range_end(compose_definition) */
+    });
+    describe('関数合成', () => {
+      /* #@range_begin(compose_definition) */
+      var compose = (f) => {
+        return (g) => {
+          return (arg) =>{
+            return f(g(arg));
+          };
+        };
+      };
+      // var compose = (f) => {
+      //    var self = this;
+      //    return (g) => {
+      //      return (_) => {
+      //        return f.call(self, g.apply(self, arguments));
+      //      };
+      //    };
+      // };
+      /* #@range_end(compose_definition) */
       it('マイナスのマイナスはプラス', (next) => {
         /* #@range_begin(composition_example_negation_twice) */
         var negate = (n) => {
@@ -241,10 +241,10 @@ describe('関数の基本', () => {
           return 2 * n;
         };
         expect(
-		  compose(increment)(decrement)(5)
-		).to.eql(
-		  5
-		);
+          compose(increment)(decrement)(5)
+        ).to.eql(
+          5
+        );
         // expect(__.compose.bind(__)(decrement)(increment)(5)).to.be(5);
         // expect(__.compose.bind(__)(increment)(increment)(5)).to.be(7);
         // // (n * 2) + 1
@@ -279,117 +279,117 @@ describe('関数の基本', () => {
       //   // );
       //   next();
       // });
-	});
-	it('リストの逆順を求める', (next) => {
-	  /* #@range_begin(list_reverse) */
+    });
+    it('リストの逆順を求める', (next) => {
+      /* #@range_begin(list_reverse) */
       var reverse = (list) => {
-		return (accumulator) => {
+        return (accumulator) => {
           return seq.match(list, {
-			empty: accumulator,  // 空のリストの場合は終了
-			cons: (head, tail) => {
+            empty: accumulator,  // 空のリストの場合は終了
+            cons: (head, tail) => {
               return reverse(tail)(seq.cons(head, accumulator))
-			},
+            },
           });
-		}
+        }
       };
-	  // toArray:: LIST -> ARRAY -> ARRAY
-	  var toArray = (list) => {
-		var toArrayAux = (list) => {
-		  return (accumulator) => {
-			return seq.match(list, {
-			  empty: accumulator,  // 空のリストの場合は終了
-			  cons: (head, tail) => {
-				return toArrayAux(tail)(accumulator.concat(head))
-			  },
-			});
-		  };
-		};
-		return toArrayAux(list)([])
-	  };
-	  /**************** テスト ****************/
-	  expect(
-		toArray(reverse(seq.cons(1, seq.cons(2,seq.empty)))(seq.empty))
-	  ).to.eql(
-		[2,1]
-	  );
-	  /* #@range_end(list_reverse) */
+      // toArray:: LIST -> ARRAY -> ARRAY
+      var toArray = (list) => {
+        var toArrayAux = (list) => {
+          return (accumulator) => {
+            return seq.match(list, {
+              empty: accumulator,  // 空のリストの場合は終了
+              cons: (head, tail) => {
+                return toArrayAux(tail)(accumulator.concat(head))
+              },
+            });
+          };
+        };
+        return toArrayAux(list)([])
+      };
+      /**************** テスト ****************/
+      expect(
+        toArray(reverse(seq.cons(1, seq.cons(2,seq.empty)))(seq.empty))
+      ).to.eql(
+        [2,1]
+      );
+      /* #@range_end(list_reverse) */
       next();
-	});
+    });
   });
   describe('関数の適用', () => {
-	
+    
   });
   describe('関数の純粋性', () => {
-	describe('副作用が参照透過性を損なうこと', () => {
-	  it('console.logが参照透過性を損なうこと', (next) => {
-		/* #@range_begin(log_destroys_referential_transparency) */
-		expect(
-		  console.log("this is a test")
-		).to.eql(
-		  console.log("this is anoter test")
-		)
-		/* #@range_end(log_destroys_referential_transparency) */
-		next();
-	  });
-	  describe('ファイル操作が参照透過性を損なうこと', () => {
-		/* #@range_begin(fileio_destroys_referential_transparency) */
-		var fs = require('fs');
-		after(() => { // テストの終了時に実行される
-		  fs.writeFileSync('test/resources/file.txt', "This is a test.");
-		});
-		it('ファイルを操作する', (next) => {
-		  expect(
-			fs.readFileSync("test/resources/file.txt", 'utf8')
-		  ).to.be(
-			"This is a test."
-		  );
-		  fs.writeFileSync('test/resources/file.txt', "This is another test.")
-		  expect(
-		    fs.readFileSync("test/resources/file.txt", 'utf8')
-		  ).not.to.be(
-		    "This is a test."
-		  );
-		  next();
-		});
-		/* #@range_end(fileio_destroys_referential_transparency) */
-	  });
-	});
-	describe('副作用の分離', () => {
-	  it('kestrelコンビネーター', (next) => {
-		// this.timeout(2000);
-		// var timeout = setTimeout(next, 2000);
-		/* #@range_begin(kestrel_combinator) */
-		var kestrel = (x) => {
-		  return (y) => {
-			return x;
-		  };
-		};
-		/* #@range_end(kestrel_combinator) */
-		/* #@range_begin(kestrel_combinator_test) */
-		expect(
-		  kestrel(1)(2)
-		).to.eql(
-		  1
-		);
-		/*
-		expect(
-		  kestrel(1)(infiniteLoop())
-		).to.eql(
-		  1
-		);
-		*/ 
-		/* #@range_end(kestrel_combinator_test) */
-		// setTimeout(function(){
+    describe('副作用が参照透過性を損なうこと', () => {
+      it('console.logが参照透過性を損なうこと', (next) => {
+        /* #@range_begin(log_destroys_referential_transparency) */
+        expect(
+          console.log("this is a test")
+        ).to.eql(
+          console.log("this is anoter test")
+        )
+        /* #@range_end(log_destroys_referential_transparency) */
+        next();
+      });
+      describe('ファイル操作が参照透過性を損なうこと', () => {
+        /* #@range_begin(fileio_destroys_referential_transparency) */
+        var fs = require('fs');
+        after(() => { // テストの終了時に実行される
+          fs.writeFileSync('test/resources/file.txt', "This is a test.");
+        });
+        it('ファイルを操作する', (next) => {
+          expect(
+            fs.readFileSync("test/resources/file.txt", 'utf8')
+          ).to.be(
+            "This is a test."
+          );
+          fs.writeFileSync('test/resources/file.txt', "This is another test.")
+          expect(
+            fs.readFileSync("test/resources/file.txt", 'utf8')
+          ).not.to.be(
+            "This is a test."
+          );
+          next();
+        });
+        /* #@range_end(fileio_destroys_referential_transparency) */
+      });
+    });
+    describe('副作用の分離', () => {
+      it('kestrelコンビネーター', (next) => {
+        // this.timeout(2000);
+        // var timeout = setTimeout(next, 2000);
+        /* #@range_begin(kestrel_combinator) */
+        var kestrel = (x) => {
+          return (y) => {
+            return x;
+          };
+        };
+        /* #@range_end(kestrel_combinator) */
+        /* #@range_begin(kestrel_combinator_test) */
+        expect(
+          kestrel(1)(2)
+        ).to.eql(
+          1
+        );
+        /*
+        expect(
+          kestrel(1)(infiniteLoop())
+        ).to.eql(
+          1
+        );
+        */ 
+        /* #@range_end(kestrel_combinator_test) */
+        // setTimeout(function(){
         //   // happens 0.5 seconds later:
-		//   expect(
-		// 	kestrel(1)(infiniteLoop())
-		//   ).to.eql(
-		// 	1
-		//   );
+        //   expect(
+        //  kestrel(1)(infiniteLoop())
+        //   ).to.eql(
+        //  1
+        //   );
         //   next(); // this tells mocha to run the next test
-		// },500);
-		next();
-	  });
-	});
+        // },500);
+        next();
+      });
+    });
   });
 });

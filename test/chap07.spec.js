5199bffd377e3c0289b6772297c4df718e0f618c
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2015-11-23 21:25:56 +0900

    implemented continuation passing interpreter

--- test/chap07.spec.js
+++ test/chap07.spec.js
@@ -2,6 +2,10 @@
 
 var expect = require('expect.js');
 
+var match = (exp, pattern) => {
+  return exp.call(pattern, pattern);
+};
+
 // 関数型言語を作る
 // ========
 describe('関数型言語を作る', () => {
@@ -28,15 +32,15 @@ describe('関数型言語を作る', () => {
       };
     };
     /* #@range_end(environment) */
-	describe('環境をテストする', () => {
+    describe('環境をテストする', () => {
       it('extendEnvで環境を作り、 lookupEnv で環境を探る', (next) => {
-		var newEnv = extendEnv('a',1, emptyEnv);
-		expect(
+        var newEnv = extendEnv('a',1, emptyEnv);
+        expect(
           lookupEnv("a", newEnv)
-		).to.be(
+        ).to.be(
           1
-		);
-		next();
+        );
+        next();
       });
     });
     describe('式を作る', () => {
@@ -138,13 +142,14 @@ describe('関数型言語を作る', () => {
             next();
           });
           it('constant関数', (next) => {
-            var constant = lambda(variable("x"),number(1))
+            // λx.1
+            var constant = lambda(variable("x"),number(1));
             expect(
               evaluate(constant, emptyEnv)
             ).to.a(
               'function'
             );
-            var applied = application(constant, variable("y"))
+            var applied = application(constant, variable("y"));
             expect(
               evaluate(applied, emptyEnv)
             ).to.eql(
@@ -152,16 +157,16 @@ describe('関数型言語を作る', () => {
             );
             next();
           });
-          it('id関数', (next) => {
+          it('identity関数', (next) => {
             /* λx.x */
-            var id = lambda(variable("x"),variable("x"));
+            var identity = lambda(variable("x"),variable("x"));
             expect(
-              evaluate(id, emptyEnv)
+              evaluate(identity, emptyEnv)
             ).to.a(
               'function'
             );
             expect(
-              evaluate(application(id, number(1)), emptyEnv)
+              evaluate(application(identity, number(1)), emptyEnv)
             ).to.eql(
               1
             );
@@ -242,138 +247,363 @@ describe('関数型言語を作る', () => {
         });
       });
     }); // 
+    // describe('式と値を分離した評価器', () => {
+    //   // ## 式の代数的データ構造
+    //   var exp = {
+    //     number: (value) => {
+    //       expect(value).to.a('number');
+    //       return (pattern) => {
+    //         return pattern.number(value);
+    //       };
+    //     },
+    //     variable: (name) => {
+    //       expect(name).to.a('string');
+    //       return (pattern) => {
+    //         return pattern.variable(name);
+    //       };
+    //     },
+    //     lambda: (variable, body) => {
+    //       expect(variable).to.a('function');
+    //       expect(body).to.a('function');
+    //       return (pattern) => {
+    //         return pattern.lambda(variable, body);
+    //       };
+    //     },
+    //     application: (variable, arg) => {
+    //       return (pattern) => {
+    //         return pattern.application(variable, arg);
+    //       };
+    //     }
+    //   }; // exp
+    //   // RESULT
+    //   var result = {
+    //     value: (answer) => {
+    //       return (pattern) => {
+    //         return pattern.value(answer);
+    //       };
+    //     },
+    //     // closure: (func) => {
+    //     //   return (pattern) => {
+    //     //  return pattern.closure(func);
+    //     //   };
+    //     // }
+    //   };
+    //   var evaluate = (exp, env) => {
+    //     return match(exp,{
+    //       /* 数値の評価 */
+    //       number: (value) => {
+    //         return result.value(value);
+    //       },
+    //       /* 変数の評価 */
+    //       variable: (name) => {
+    //         return result.value(lookupEnv(name, env));
+    //       },
+    //       /* λ式の評価 */
+    //       lambda: (variable, bodyExp) => {
+    //         /* クロージャーを返す */
+    //         return result.value((actualArg) => {
+    //           return match(variable,{
+    //             variable: (name) => {
+    //               return match(evaluate(bodyExp, extendEnv(name, actualArg ,env)),{
+    //                 value: (value) => {
+    //                   return value;
+    //                 }
+    //               });
+    //             }
+    //           });
+    //         });
+    //       },
+    //       /* 関数適用の評価 */
+    //       application: (func, arg) => {
+    //         return match(evaluate(func, env),{
+    //           value: (closure) => {
+    //             var rand = evaluate(arg, env);
+    //             return closure(rand);
+    //           }
+    //         });
+    //       }
+    //     });
+    //   };
+    //   it('数値を評価する', (next) => {
+    //     match(evaluate(exp.number(2), emptyEnv), {
+    //       value: (answer) => {
+    //         expect(answer).to.eql(2);
+    //       },
+    //       abort: (message) => {
+    //         expect().fail();
+    //       }
+    //     });
+    //     next();
+    //   });
+    //   it('変数を評価する', (next) => {
+    //     var env = extendEnv("x",1, emptyEnv);
+	//   	match(evaluate(exp.variable("x"), env), {
+	//   	  value: (answer) => {
+	//   		expect(answer).to.eql(1);
+	//   	  }
+	//   	});
+	//   	// 自由変数の場合
+	//   	match(evaluate(exp.variable("y"), env), {
+	//   	  value: (answer) => {
+	//   		expect(answer).to.eql(undefined);
+	//   	  }
+	//   	});
+    //     next();
+    //   });
+    //   it('constant関数', (next) => {
+    //     // λx.1
+    //     var constant = exp.lambda(exp.variable("x"),exp.number(1));
+    //     expect(
+    //       evaluate(constant, emptyEnv)
+    //     ).to.a(
+    //       'function'
+    //     );
+    //     // (λx.1)(2)
+    //     var appliedExpression = exp.application(constant, exp.number(2));
+	//   	match(evaluate(appliedExpression, emptyEnv), {
+	//   	  value: (answer) => {
+	//   		expect(answer).to.eql(undefined);
+	//   	  }
+	//   	});
+    //     next();
+    //   });
+    // }); // 式と値を分離した評価器
     describe('継続渡し評価器', () => {
       // ## 式の代数的データ構造
       var match = (exp, pattern) => {
         return exp.call(pattern, pattern);
       };
-	  var exp = {
-		raise: (exp) => {
+      var exp = {
+        raise: (exp) => {
           return (pattern) => {
-			return pattern.raise(exp);
+            return pattern.raise(exp);
           };
-		},
-		tryWith: (exp, exception, raisedExp) => {
+        },
+        tryWith: (exp, exception, raisedExp) => {
           return (pattern) => {
-			return pattern.tryWith(exp, exception, raisedExp);
+            return pattern.tryWith(exp, exception, raisedExp);
           };
-		},
-		number: (value) => {
+        },
+        number: (value) => {
           expect(value).to.a('number');
           return (pattern) => {
-			return pattern.number(value);
+            return pattern.number(value);
           };
-		},
-		variable: (name) => {
+        },
+        variable: (name) => {
           expect(name).to.a('string');
           return (pattern) => {
-			return pattern.variable(name);
+            return pattern.variable(name);
           };
-		},
-		lambda: (variable, body) => {
+        },
+        lambda: (variable, body) => {
           expect(variable).to.a('function');
           expect(body).to.a('function');
           return (pattern) => {
-			return pattern.lambda(variable, body);
+            return pattern.lambda(variable, body);
           };
-		},
-		application: (variable, arg) => {
+        },
+        application: (variable, arg) => {
           return (pattern) => {
-			return pattern.application(variable, arg);
+            return pattern.application(variable, arg);
           };
-		}
-	  }; // exp
-	  var result = {
-		value: (answer) => {
-          return (pattern) => {
-			return pattern.value(answer);
-          };
-		},
-		abort: (message) => {
-          return (pattern) => {
-			return pattern.abort(message);
-          };
-		}
-	  };
-	  // ## 式の評価関数
-	  // evaluateCPS: (EXP, ENV, FUNC[VALUE -> RESULT]) -> RESULT
+        }
+      }; // exp
+      // ## 式の評価関数
+      // evaluateCPS: (EXP, ENV, FUNC[VALUE -> RESULT]) -> RESULT
       var evaluateCPS = (exp, env, continues) => {
         return match(exp,{
           /* 例外の評価 */
           raise: (exp) => {
-            return result.abort("exception raised");
+            return new Error();
           },
           /* 数値の評価 */
           number: (answer) => {
-            return continues(result.value(answer));
+            return continues(answer);
           },
           /* 変数の評価 */
           variable: (name) => {
-			var found = lookupEnv(name, env);
+            var found = lookupEnv(name, env);
             if(found === undefined){
-			  return result.abort(name + " not found");
-			} else {
-			  return continues(result.value(found));
-			}
+              return new Error(name + " not found");
+            } else {
+              return continues(found);
+            }
           },
           /* λ式の評価 */
-          lambda: (variable, bodyExp) => {
+          lambda: (exp, bodyExp) => {
             /* クロージャーを返す */
             return (actualArg) => {
-              return match(variable,{ // maybeを返すべきか？
+              return match(exp,{
                 variable: (name) => {
-                  return continues(evaluateCPS(bodyExp, extendEnv(name, actualArg ,env)));
+                  return continues(evaluateCPS(bodyExp, extendEnv(name, actualArg ,env), continues));
+                },
+                number: (value) => {
+                  return new Error("lambdaの引数が数値になっています");
+                },
+                lambda: ($$,$$$) => {
+                  return new Error("lambdaの引数がlambbaになっています");
                 }
               });
             };
           },
           /* 関数適用の評価 */
-          application: (variable, arg) => {
-            var rator = evaluateCPS(variable, env);
-            var rand = evaluateCPS(arg, env);
+          application: (exp, arg) => {
+            var rator = evaluateCPS(exp, env, continues);
+            var rand = evaluateCPS(arg, env, continues);
             return continues(rator(rand));
+            
           }
         });
-      };
+	  };
 	  describe('継続渡し評価器で式を評価する', () => {
-		var returns = (result) => {
-		  return result;
-		};
+	  	var returns = (result) => {
+	  	  return result;
+	  	};
         it('数値を評価する', (next) => {
-		  match(evaluateCPS(exp.number(2), emptyEnv, returns), {
-			value: (answer) => {
-			  expect(answer).to.eql(2);
-			},
-			abort: (message) => {
-			  expect().fail();
-			}
-		  });
-		  next();
+          expect(
+            evaluateCPS(exp.number(2), emptyEnv, returns)
+          ).to.eql(2);
+	  	  // match(evaluateCPS(exp.number(2), emptyEnv, returns), {
+	  	  //   value: (answer) => {
+	  	  //     expect(answer).to.eql(2);
+	  	  //   },
+	  	  //   abort: (message) => {
+	  	  //     expect().fail();
+	  	  //   }
+	  	  // });
+	  	  next();
         });
         it('変数を評価する', (next) => {
           var env = extendEnv("x",1, emptyEnv, returns);
-		  match(evaluateCPS(exp.variable("x"), env, returns), {
-			value: (answer) => {
-			  expect(answer).to.eql(1);
-			},
-			abort: (message) => {
-			  expect().fail();
-			}
-		  });
-		  match(evaluateCPS(exp.variable("y"), env, returns), {
-			value: (answer) => {
-			  expect().fail();
+	  	  expect(
+            evaluateCPS(exp.variable("x"), env, returns)
+          ).to.eql(
+            1
+          );
+	  	  // match(evaluateCPS(exp.variable("x"), env, returns), {
+	  	  //   value: (answer) => {
+	  	  //     expect(answer).to.eql(1);
+	  	  //   },
+	  	  //   abort: (message) => {
+	  	  //     expect().fail();
+	  	  //   }
+	  	  // });
+	  	  // 自由変数の場合は、 abortが返る
+	  	  expect(
+            evaluateCPS(exp.variable("y"), env, returns)
+          ).to.eql(
+            new Error("y not found")
+          );
+	  	  // match(evaluateCPS(exp.variable("y"), env, returns), {
+	  	  //   value: (answer) => {
+	  	  //     expect().fail();
 			  
-			},
-			abort: (message) => {
-			  expect(message).to.eql("y not found");
-			}
-		  });
+	  	  //   },
+	  	  //   abort: (message) => {
+	  	  //     expect(message).to.eql("y not found");
+	  	  //   }
+	  	  // });
           next();
         });
-	  });
+        it('constant関数', (next) => {
+          var constant = exp.lambda(exp.variable("x"),exp.number(1));
+          expect(
+            evaluateCPS(constant, emptyEnv, returns)
+          ).to.a(
+            'function'
+          );
+	  	  // (λx.1)(2)
+          var applied = exp.application(constant, exp.number(2));
+	  	  expect(
+            evaluateCPS(applied, emptyEnv, returns)
+          ).to.eql(
+            1
+          );
+          next();
+        });
+        it('identity関数をテストする', (next) => {
+          /* λx.x */
+          var identity = exp.lambda(exp.variable("x"),exp.variable("x"));
+          expect(
+            evaluateCPS(identity, emptyEnv, returns)
+          ).to.a(
+            'function'
+          );
+	  	  // (λx.x)(1) = 1 */
+	  	  var appliedExpression = exp.application(identity, exp.number(1));
+	  	  expect(
+            evaluateCPS(appliedExpression, emptyEnv, returns)
+          ).to.eql(
+            1
+          );
+          next();
+        });
+        it('ブール型を評価する', (next) => {
+          this.timeout(1000);
+          /* λx.λy.x */
+          var trueFun = exp.lambda(exp.variable("x"),exp.lambda(exp.variable("y"),exp.variable("x")));
+          /* λx.λy.y */
+          var falseFun = exp.lambda(exp.variable("x"),exp.lambda(exp.variable("y"),exp.variable("y")));
+          var not = exp.lambda(exp.variable("x"),
+                           exp.application(
+                             exp.application(
+                               exp.variable("x"),falseFun),
+                             trueFun));
+          var and = exp.lambda(exp.variable("x"),
+                           exp.lambda(exp.variable("y"),
+                                  exp.application(
+                                    exp.application(exp.variable("x"),exp.variable("y")),
+                                    falseFun)));
+          var or = exp.lambda(exp.variable("x"),
+                          exp.lambda(exp.variable("y"),
+                                 exp.application(
+                                   exp.application(exp.variable("x"),trueFun),
+                                   exp.variable("y"))));
+          var cond = exp.lambda(exp.variable("pred"),
+                            exp.lambda(exp.variable("x"),
+                                   exp.lambda(exp.variable("y"),
+                                          exp.application(
+                                            exp.application(exp.variable("pred"),exp.variable("x")),exp.variable("y")))));
+          // (λx.λy.x)(1)(0) = 1
+          expect(
+            evaluateCPS(
+              exp.application(
+                exp.application(trueFun,exp.number(1)),
+                exp.number(0)),
+              emptyEnv,
+              returns)
+          ).to.eql(
+            1
+          );
+          // (λx.λy.x)(1)(z) = 1
+          expect(
+            evaluateCPS(
+              exp.application(
+                exp.application(trueFun,exp.number(1)),
+                exp.variable("z")),
+              emptyEnv,
+              returns)
+          ).to.eql(
+            1
+          );
+          // (λx.λy.x)(z)(0) = error
+          expect(
+            evaluateCPS(
+              exp.application(
+                exp.application(trueFun,exp.variable("z")),
+                exp.number(0)),
+              emptyEnv,
+              returns)
+          ).to.eql(
+            new Error("z not found")
+          );
+          next();
+        });
+      });
     });
   });
+});
+
   // describe('失敗継続渡し評価器', () => {
   // });
-});

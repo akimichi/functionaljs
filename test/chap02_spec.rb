5334b9a36a71c4e89d4ae46b812c7652c91e9c6b
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2016-03-31 07:32:45 +0900

    add ruby class test

--- test/chap02_spec.rb
+++ test/chap02_spec.rb
@@ -17,4 +17,18 @@ describe '第2章' do
            adder[1][2]
            ).to eq 3
   end
+  it 'オブジェクト指向プログラミングのテスト' do
+    class User
+      @@count = 0
+      def initialize(name)
+        @name = name
+        @@count = @@count + 1
+      end
+      attr_accessor :name
+      def self.count
+        @@count
+      end
+    end
+    expect(User.count).to eq 0
+  end
 end

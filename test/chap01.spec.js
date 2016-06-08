586bef6e94fc6f4dc567d342e9cfb3bd5cd8aee0
Moe Masuko <moe@topstudio.co.jp>
2016-06-08 15:32:35 +0900

    cha01: インデントなど調整

--- test/chap01.spec.js
+++ test/chap01.spec.js
@@ -14,36 +14,36 @@ describe('命令型モデル', () => {
     /* c.f. http://swizec.com/blog/a-turing-machine-in-133-bytes-of-javascript/swizec/3069 */
     // [![IMAGE ALT TEXT](http://img.youtube.com/vi/lf1KZmYOG_g/0.jpg)](https://www.youtube.com/watch?v=lf1KZmYOG_g "チューリングマシンの動画")
     /* #@range_begin(turing) */
-    var machine = (program,tape,initState, endState) => {
-      /* ヘッドの位置 */
-      var position = 0;                          
-      /* 機械の状態 */
-      var state = initState;                     
-      /* 実行する命令 */
-      var currentInstruction = undefined;        
-      /*
-        以下のwhileループにて、
-        現在の状態が最終状態に到達するまで命令を繰り返す
-      */
-      while(state != endState) {
-        var cell = tape[String(position)];
-        if (cell)
-          currentInstruction = program[state][cell];
-        else
-          currentInstruction = program[state].B;
-        if (!currentInstruction) {
-          return false;
-        } else {
-          /* テープに印字する */
-          tape[String(position)] = currentInstruction.write; 
-          /* ヘッドを動かす */
-          position += currentInstruction.move;                
-          /* 次の状態に移る */
-          state = currentInstruction.next;                    
+      var machine = (program, tape, initState, endState) => {
+        /* ヘッドの位置 */
+        var position = 0;                          
+        /* 機械の状態 */
+        var state = initState;                     
+        /* 実行する命令 */
+        var currentInstruction = undefined;        
+        /*
+          以下のwhileループにて、
+          現在の状態が最終状態に到達するまで命令を繰り返す
+        */
+        while (state != endState) {
+          var cell = tape[String(position)];
+          if (cell)
+            currentInstruction = program[state][cell];
+          else
+            currentInstruction = program[state].B;
+          if (!currentInstruction) {
+            return false;
+          } else {
+            /* テープに印字する */
+            tape[String(position)] = currentInstruction.write; 
+            /* ヘッドを動かす */
+            position += currentInstruction.move;                
+            /* 次の状態に移る */
+            state = currentInstruction.next;                    
+          }
         }
-      }
-      return tape;
-    };
+        return tape;
+      };
     /* #@range_end(turing)  */
     // ### チューリング機械のテスト
     describe('チューリング機械をテストする', () => {
@@ -74,7 +74,7 @@ describe('命令型モデル', () => {
         expect(
           result
         ).to.eql(
-          ['1','1','1','0','1','1','1' ]
+          ['1', '1', '1', '0', '1', '1', '1' ]
         );
         /* #@range_end(turing_example) */
         next();
@@ -91,7 +91,7 @@ describe('命令型モデル', () => {
                   'q0',       // 初期状態
                   'q1')       // 終了状態
         ).to.eql(
-          ['0','B']
+          ['0', 'B']
         );
         expect(
           machine(program,    // プログラム
@@ -99,48 +99,48 @@ describe('命令型モデル', () => {
                   'q0',       // 初期状態
                   'q1')       // 終了状態
         ).to.eql(
-          ['1','B']
+          ['1', 'B']
         );
         next();
       });
       it('succ関数を実装する', (next) => {
         /* #@range_begin(turing_example_succ) */
-        /* 2進法で10、10進法で2を表わすテープ */
-        var tape = { 
-          '0':'1',
-          '1':'0'
-        };
-        /* チューリング機械に与えるプログラム */
-        var program = {
-          'q0': {"1": {"write": "1", "move": 1, "next": 'q0'},
-                 "0": {"write": "0", "move": 1, "next": 'q0'},
-                 "B": {"write": "B", "move": -1, "next": 'q1'}},
-          'q1': {"1": {"write": "0", "move": -1, "next": 'q1'},
-                 "0": {"write": "1", "move": -1, "next": 'q2'},
-                 "B": {"write": "1", "move": -1, "next": 'q3'}},
-          'q2': {"1": {"write": "1", "move": -1, "next": 'q2'},
-                 "0": {"write": "0", "move": -1, "next": 'q2'},
-                 "B": {"write": "B", "move": 1, "next": 'q4'}},
-          'q3': {"1": {"write": "1", "move": 1, "next": 'q4'},
-                 "0": {"write": "0", "move": 1, "next": 'q4'},
-                 "B": {"write": "B", "move": 1, "next": 'q4'}}
-        };
+      /* 2進法で10、10進法で2を表わすテープ */
+      var tape = { 
+        '0': '1',
+        '1': '0'
+      };
+      /* チューリング機械に与えるプログラム */
+      var program = {
+        'q0': {"1": {"write": "1", "move": 1, "next": 'q0'},
+               "0": {"write": "0", "move": 1, "next": 'q0'},
+               "B": {"write": "B", "move": -1, "next": 'q1'}},
+        'q1': {"1": {"write": "0", "move": -1, "next": 'q1'},
+               "0": {"write": "1", "move": -1, "next": 'q2'},
+               "B": {"write": "1", "move": -1, "next": 'q3'}},
+        'q2': {"1": {"write": "1", "move": -1, "next": 'q2'},
+               "0": {"write": "0", "move": -1, "next": 'q2'},
+               "B": {"write": "B", "move": 1, "next": 'q4'}},
+        'q3': {"1": {"write": "1", "move": 1, "next": 'q4'},
+               "0": {"write": "0", "move": 1, "next": 'q4'},
+               "B": {"write": "B", "move": 1, "next": 'q4'}}
+      };
         /* #@range_end(turing_example_succ) */
         expect(
-          /* #@range_begin(turing_example_succ_test) */
-          machine(program,     // プログラム
-                  tape,        // テープ
-                  'q0',        // 初期状態
-                  'q4')        // 終了状態
+      /* #@range_begin(turing_example_succ_test) */
+      machine(program,     // プログラム
+              tape,        // テープ
+              'q0',        // 初期状態
+              'q4')        // 終了状態
           /* #@range_end(turing_example_succ_test) */
         ).to.eql(
           /* #@range_begin(turing_example_succ_test_result) */
-          {
-            '-1': 'B',
-            '0': '1',
-            '1': '1',
-            '2': 'B'
-          }
+      {
+        '-1': 'B',
+        '0': '1',
+        '1': '1',
+        '2': 'B'
+      }
           /* #@range_end(turing_example_succ_test_result) */
         );
         // #### <a name="turing_demo"> **チューリング機械の挙動** </a>
@@ -208,7 +208,7 @@ describe('関数型モデル', () => {
     });
     it('sumsquaresとsquareの簡約', (next) => {
       /* #@range_begin(sumsquares) */
-      var sumsquares = (x,y) => {
+      var sumsquares = (x, y) => {
         return square(x) + square(y);
       };
       var square = (x) => {
@@ -216,7 +216,7 @@ describe('関数型モデル', () => {
       };
       /* #@range_end(sumsquares) */
       expect(
-        sumsquares(3,4)
+        sumsquares(3, 4)
       ).to.eql(
         25
       );
@@ -231,8 +231,8 @@ describe('関数型モデル', () => {
         return n - 1;
       };
       /* add関数の定義 */
-      var add = (x,y) => { 
-        if(y < 1){
+      var add = (x, y) => { 
+        if (y < 1) {
           return x;
         } else {
           /* add関数の再帰呼び出し */
@@ -243,7 +243,7 @@ describe('関数型モデル', () => {
       // #### <a name="add_reduction_demo"> **add(3,2)の簡約** </a>
       // ![add関数の簡約](images/add_reduction.gif) 
       expect(
-        add(2,1)
+        add(2, 1)
       ).to.eql(
         3
       );
@@ -252,10 +252,10 @@ describe('関数型モデル', () => {
     it('再帰と漸化式', (next) => {
       /* #@range_begin(recursion) */
       var a = (n) => {
-        if(n === 1) {
-          return 1; // 初項は1
+        if (n === 1) {
+          return 1;  // 初項は1
         } else {
-          return a(n-1) + 3; // 公差は3
+          return a(n-1) + 3;  // 公差は3
         }
       };
       /* #@range_end(recursion) */
@@ -324,22 +324,22 @@ describe('関数型モデル', () => {
         return n - 1;
       };
       var add = (x, y) => { // add関数の定義
-        if(y < 1){
+        if (y < 1) {
           return x;
         } else {
-          return add(succ(x),prev(y)); // add関数の再帰呼び出し
+          return add(succ(x), prev(y)); // add関数の再帰呼び出し
         }
       };
       /* #@range_begin(multiply) */
-      var times = (count,fun,arg, memo) => {
-        if(count > 1) {
-          /* times関数を再帰呼出し */
-          return times(count-1,fun,arg, fun(memo,arg)); 
+      var times = (count, fun, arg, memo) => {
+        if (count > 1) {
+          /* times関数を再帰呼び出し */
+          return times(count-1, fun, arg, fun(memo, arg)); 
         } else {
-          return fun(memo,arg);
+          return fun(memo, arg);
         }
       };
-      var multiply = (n,m) => {
+      var multiply = (n, m) => {
         /* 2番目の引数にadd関数を渡している */
         return times(m, add, n, 0); 
       };
@@ -347,27 +347,27 @@ describe('関数型モデル', () => {
       // #### <a name="multiply_reduction_demo"> **multiply(2,3)の簡約** </a>
       // ![multiply関数の簡約](images/multiply_reduction.gif) 
       expect(
-        add(2,3)
+        add(2, 3)
       ).to.eql(
         5
       );
       expect(
-        multiply(2,3)
+        multiply(2, 3)
       ).to.eql(
         6
       );
       expect(
-        multiply(4,6)
+        multiply(4, 6)
       ).to.eql(
         24
       );
       expect(
-        multiply(1,1)
+        multiply(1, 1)
       ).to.eql(
         1
       );
       expect(
-        multiply(0,1)
+        multiply(0, 1)
       ).to.eql(
         0
       );
@@ -382,29 +382,29 @@ describe('関数型モデル', () => {
       return n - 1;
     };
     var add = (x, y) => {
-      if(y < 1){
+      if (y < 1) {
         return x;
       } else {
-        return add(succ(x),prev(y));
+        return add(succ(x), prev(y));
       }
     };
-    var times = (count,fun,arg, memo) => {
-      if(count > 1) {
-        return times(count-1,fun,arg, fun(arg,memo));
+    var times = (count, fun, arg, memo) => {
+      if (count > 1) {
+        return times(count-1, fun, arg, fun(arg, memo));
       } else {
-        return fun(arg,memo);
+        return fun(arg, memo);
       }
     };
-    var multiply = (n,m) => {
+    var multiply = (n, m) => {
       return times(m, add, n, 0);
     };
     /* #@range_begin(exponential) */
-    var exponential = (n,m) => {
-      return times(m, multiply, n, 1);
-    };
+      var exponential = (n, m) => {
+        return times(m, multiply, n, 1);
+      };
     /* #@range_end(exponential) */
     expect(
-      exponential(2,3)
+      exponential(2, 3)
     ).to.eql(
       8
     );

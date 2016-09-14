be99224fc9e5c03561e025812749813a273da01c
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2016-09-15 00:06:19 +0900

    第１章の2回目見直しを終了

--- test/chap01.spec.js
+++ test/chap01.spec.js
@@ -1,17 +1,29 @@
 "use strict";
 
+// ## 目次
+// > * [命令型モデル](http://akimichi.github.io/functionaljs/chap01.spec.html#imperative_model)
+// >   - [チューリング機械](http://akimichi.github.io/functionaljs/chap01.spec.html#turing_machine)
+// > * [関数型モデル](http://akimichi.github.io/functionaljs/chap01.spec.html#functional_model)
+// >   - [置換ルール](http://akimichi.github.io/functionaljs/chap01.spec.html#substitution_model)
+// >   - [関数の定義](http://akimichi.github.io/functionaljs/chap01.spec.html#function_definition)
+// >   - [関数型モデルを使うメリット](http://akimichi.github.io/functionaljs/chap01.spec.html#advantages_of_functional_model)
+
 // 第1章 「計算」とは
 // ========
+
 
 /* アサートライブラリー expect.js の読み込み */
 var expect = require('expect.js');
 
-// ## 1.2 命令型モデル
+// ## 1.2 <section id='imperative_model'>命令型モデル</section>
 describe('命令型モデル', () => {
-  // ### チューリング機械
+  // ###  <section id='turing_machine'>チューリング機械</section>
   describe('チューリング機械 turing machine', () => {
-    // c.f. [A turing machine in 133 bytes of javascript](http://swizec.com/blog/a-turing-machine-in-133-bytes-of-javascript/swizec/3069) 
+    // チューリング機械を現実の装置として製作したもの
     // [![IMAGE ALT TEXT](http://img.youtube.com/vi/E3keLeMwfHY/0.jpg)](https://www.youtube.com/watch?v=E3keLeMwfHY "チューリングマシンの動画")
+    /* [A turing machine in 133 bytes of javascript](http://swizec.com/blog/a-turing-machine-in-133-bytes-of-javascript/swizec/3069) を参考にしている */
+
+    // JavaScriptによるチューリング機械
     /* #@range_begin(turing) */
     var machine = (program,tape,initState, endState) => {
       /* ヘッドの位置 */
@@ -44,9 +56,9 @@ describe('命令型モデル', () => {
       return tape;
     };
     /* #@range_end(turing)  */
-    // ### チューリング機械のテスト
-    describe('チューリング機械をテストする', () => {
-      // #### チューリング機械でsucc関数を実装する
+
+    // ### チューリング機械の実行例
+    describe('チューリング機械の実行例', () => {
       // #### <a name="turing_demo"> **チューリング機械の挙動** </a>
       // ![チューリング機械の挙動](images/turing_succ.gif) 
       it('チューリング機械でsucc関数を実装する', (next) => {
@@ -91,44 +103,12 @@ describe('命令型モデル', () => {
         );
         next();
       });
-      it('チューリング機械で入力を複製する', (next) => {
-        /* #@range_begin(turing_example) */
-        var program = {
-          'q0': {"1": {"write": "B", "move": 1, "next": 'q1'}},
-          'q1': {"1": {"write": "1", "move": 1, "next": 'q1'},
-                 "0": {"write": "0", "move": 1, "next": 'q2'},
-                 "B": {"write": "0", "move": 1, "next": 'q2'}},
-          'q2': {"1": {"write": "1", "move": 1, "next": 'q2'},
-                 "B": {"write": "1", "move": -1, "next": 'q3'}},
-          'q3': {"1": {"write": "1", "move": -1, "next": 'q3'},
-                 "0": {"write": "0", "move": -1, "next": 'q3'},
-                 "B": {"write": "1", "move": 1, "next": 'q4'}},
-          'q4': {"1": {"write": "B", "move": 1, "next": 'q1'},
-                 "0": {"write": "0", "move": 1, "next": 'q5'}}
-        };
-        var tape = {
-          '0': '1',
-          '1':'1',
-          '2':'1'
-        };
-        var result = machine(program,     // プログラム
-                             tape,        // テープ
-                             'q0',        // 初期状態
-                             'q5');       // 終了状態
-        expect(
-          result
-        ).to.eql(
-          ['1','1','1','0','1','1','1' ]
-        );
-        /* #@range_end(turing_example) */
-        next();
-      });
     });
   });
 });
-// ## 1.3 関数型モデル
+// ## 1.3 <section id='functional_model'>関数型モデル</section>
 describe('関数型モデル', () => {
-  // ### 置換ルール
+  // ###  <section id='substitution_model'>置換ルール</section>
   describe('置換ルール', () => {
     it('単純なλ式の簡約', (next) => {
       /* #@range_begin(succ) */
@@ -143,8 +123,8 @@ describe('関数型モデル', () => {
       );
       next();
     });
-    // #### 再帰関数としてのadd
-    it('再帰関数としてのadd', (next) => {
+    // add関数
+    it('add関数', (next) => {
       /* #@range_begin(recursive_add) */
       var succ = (n) => {
         return n + 1;
@@ -162,57 +142,12 @@ describe('関数型モデル', () => {
         }
       };
       /* #@range_end(recursive_add) */
-      // #### <a name="add_reduction_demo"> **add(3,2)の簡約** </a>
+      // <a name="add_reduction_demo"> add(3,2)の簡約 </a>
       // ![add関数の簡約](images/add_reduction.gif) 
       expect(
         add(2,1)
       ).to.eql(
         3
-      );
-      next();
-    });
-    it('sumsquaresとsquareの簡約', (next) => {
-      /* #@range_begin(sumsquares) */
-      var sumsquares = (x,y) => {
-        return square(x) + square(y);
-      };
-      var square = (x) => {
-        return x * x;
-      };
-      /* #@range_end(sumsquares) */
-      expect(
-        sumsquares(3,4)
-      ).to.eql(
-        25
-      );
-      next();
-    });
-    // #### 命令的なadd関数
-    it('命令的なadd関数', (next) => {
-      /* add関数の定義 */
-      /* #@range_begin(imperative_add) */
-      var add = (x,y) => { 
-        while(y > 0) {  // yが0より大きい間、反復処理を実行する
-          x = x + 1;    // 変数xを更新する 
-          y = y - 1;    // 変数yを更新する
-        }
-        return x;
-      };
-      /* #@range_end(imperative_add) */
-      expect(
-        add(1,2)
-      ).to.eql(
-        3
-      );
-      expect(
-        add(2,3)
-      ).to.eql(
-        5
-      );
-      expect(
-        add(0,2)
-      ).to.eql(
-        2
       );
       next();
     });
@@ -244,47 +179,40 @@ describe('関数型モデル', () => {
       );
       next();
     });
-    // #### 変数の置換ルール
-    it('変数の置換ルール', (next) => {
-      /* #@range_begin(variable_and_function) */
-      var foo = () => {
-        return 3;
+    // while文を利用したadd関数
+    it('while文を利用したadd関数', (next) => {
+      /* add関数の定義 */
+      /* #@range_begin(imperative_add) */
+      var add = (x,y) => { 
+        while(y > 0) {  // yが0より大きい間、反復処理を実行する
+          x = x + 1;    // 変数xを更新する 
+          y = y - 1;    // 変数yを更新する
+        }
+        return x;
       };
+      /* #@range_end(imperative_add) */
       expect(
-        foo()
+        add(1,2)
       ).to.eql(
         3
       );
-      /* #@range_end(variable_and_function) */
-      /* #@range_begin(local_variable_usage_example) */
-      var bar = () => {
-        var foo = 3;     // 変数fooに値3をバインドする
-        return foo * 10; // 変数fooの値を10倍にする
-      };
-      /* #@range_end(local_variable_usage_example) */
       expect(
-        bar()
+        add(2,3)
       ).to.eql(
-        30
+        5
       );
-      /* #@range_begin(variable_and_closure) */
-      var bar = () => {
-        var foo = 3;
-        return foo * 10;
-      };
-      var baz = ((foo) => {
-        return foo * 10;
-      });
-      /* #@range_end(variable_and_closure) */
       expect(
-        bar()
+        add(0,2)
       ).to.eql(
-        baz(3)          // 仮引数fooに値3をバインドする
+        2
       );
       next();
     });
-    // #### 関数の置換ルール
-    it('関数の置換ルール', (next) => {
+  });
+  // ### <section id='function_definition'>関数の定義</section>
+  describe('関数の定義', () => {
+    // かけ算の定義
+    it('かけ算の定義', (next) => {
       var succ = (n) => {
         return n + 1;
       };
@@ -298,7 +226,6 @@ describe('関数型モデル', () => {
           return add(succ(x),prev(y)); // add関数の再帰呼び出し
         }
       };
-      // #### かけ算の定義
       /* #@range_begin(multiply) */
       var times = (count,fun,arg, memo) => {
         if(count > 1) {
@@ -343,42 +270,45 @@ describe('関数型モデル', () => {
       next();
     });
   });
-  // #### べき乗の定義
-  it('べき乗の定義', (next) => {
-    var succ = (n) => {
-      return n + 1;
-    };
-    var prev = (n) => {
-      return n - 1;
-    };
-    var add = (x, y) => {
-      if(y < 1){
-        return x;
-      } else {
-        return add(succ(x),prev(y));
-      }
-    };
-    var times = (count,fun,arg, memo) => {
-      if(count > 1) {
-        return times(count-1,fun,arg, fun(arg,memo));
-      } else {
-        return fun(arg,memo);
-      }
-    };
-    var multiply = (n,m) => {
-      return times(m, add, n, 0);
-    };
-    /* #@range_begin(exponential) */
-    var exponential = (n,m) => {
-      return times(m, multiply, n, 1);
-    };
-    /* #@range_end(exponential) */
-    expect(
-      exponential(2,3)
-    ).to.eql(
-      8
-    );
-    next();
+  // ### <section id='advantages_of_functional_model'>関数型モデルを使うメリット</section>
+  describe('関数型モデルを使うメリット', () => {
+    // #### べき乗の定義
+    it('べき乗の定義', (next) => {
+      var succ = (n) => {
+        return n + 1;
+      };
+      var prev = (n) => {
+        return n - 1;
+      };
+      var add = (x, y) => {
+        if(y < 1){
+          return x;
+        } else {
+          return add(succ(x),prev(y));
+        }
+      };
+      var times = (count,fun,arg, memo) => {
+        if(count > 1) {
+          return times(count-1,fun,arg, fun(arg,memo));
+        } else {
+          return fun(arg,memo);
+        }
+      };
+      var multiply = (n,m) => {
+        return times(m, add, n, 0);
+      };
+      /* #@range_begin(exponential) */
+      var exponential = (n,m) => {
+        return times(m, multiply, n, 1);
+      };
+      /* #@range_end(exponential) */
+      expect(
+        exponential(2,3)
+      ).to.eql(
+        8
+      );
+      next();
+    });
   });
 });
 
---
+++ test/not_in_use/chap01.spec.js
@@ -0,0 +1,90 @@
+
+      it('チューリング機械で入力を複製する', (next) => {
+        /* #@range_begin(turing_example) */
+        var program = {
+          'q0': {"1": {"write": "B", "move": 1, "next": 'q1'}},
+          'q1': {"1": {"write": "1", "move": 1, "next": 'q1'},
+                 "0": {"write": "0", "move": 1, "next": 'q2'},
+                 "B": {"write": "0", "move": 1, "next": 'q2'}},
+          'q2': {"1": {"write": "1", "move": 1, "next": 'q2'},
+                 "B": {"write": "1", "move": -1, "next": 'q3'}},
+          'q3': {"1": {"write": "1", "move": -1, "next": 'q3'},
+                 "0": {"write": "0", "move": -1, "next": 'q3'},
+                 "B": {"write": "1", "move": 1, "next": 'q4'}},
+          'q4': {"1": {"write": "B", "move": 1, "next": 'q1'},
+                 "0": {"write": "0", "move": 1, "next": 'q5'}}
+        };
+        var tape = {
+          '0': '1',
+          '1':'1',
+          '2':'1'
+        };
+        var result = machine(program,     // プログラム
+                             tape,        // テープ
+                             'q0',        // 初期状態
+                             'q5');       // 終了状態
+        expect(
+          result
+        ).to.eql(
+          ['1','1','1','0','1','1','1' ]
+        );
+        /* #@range_end(turing_example) */
+        next();
+      });
+
+    it('sumsquaresとsquareの簡約', (next) => {
+      /* #@range_begin(sumsquares) */
+      var sumsquares = (x,y) => {
+        return square(x) + square(y);
+      };
+      var square = (x) => {
+        return x * x;
+      };
+      /* #@range_end(sumsquares) */
+      expect(
+        sumsquares(3,4)
+      ).to.eql(
+        25
+      );
+      next();
+    });
+
+    // #### 変数の置換ルール
+    it('変数の置換ルール', (next) => {
+      /* #@range_begin(variable_and_function) */
+      var foo = () => {
+        return 3;
+      };
+      expect(
+        foo()
+      ).to.eql(
+        3
+      );
+      /* #@range_end(variable_and_function) */
+      /* #@range_begin(local_variable_usage_example) */
+      var bar = () => {
+        var foo = 3;     // 変数fooに値3をバインドする
+        return foo * 10; // 変数fooの値を10倍にする
+      };
+      /* #@range_end(local_variable_usage_example) */
+      expect(
+        bar()
+      ).to.eql(
+        30
+      );
+      /* #@range_begin(variable_and_closure) */
+      var bar = () => {
+        var foo = 3;
+        return foo * 10;
+      };
+      var baz = ((foo) => {
+        return foo * 10;
+      });
+      /* #@range_end(variable_and_closure) */
+      expect(
+        bar()
+      ).to.eql(
+        baz(3)          // 仮引数fooに値3をバインドする
+      );
+      next();
+    });

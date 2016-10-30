013b31133a17e98a962c1a413b7cdd8119a98128
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2016-10-30 11:23:20 +0900

    Pretty Printer の実装を開始

--- test/pprinter.spec.js
+++ test/pprinter.spec.js
@@ -1,40 +1,18 @@
 "use strict";
 
 var expect = require('expect.js');
+var Data = require('../lib/data.js');
 var Pair = require('../lib/pair.js');
+var PP = require('../lib/pprinter.js');
 
-var PretterPrinter = {
-  pp: (data) => {
-    return data.type(data,{
-      pair: () => {
-        return "pair"; 
-      }
-    });
-  }
-};
-
-// describe('PretterPrinter', () => {
-//   it("pp", (next) => {
-//     var pair = Pair.cons(1,2);
-//     expect(
-//       Pair.self
-//     ).to.eql(
-//       pair
-//     );
-//     // expect(
-//     //   PretterPrinter.pp(pair)
-//     // ).to.eql(
-//     //   1
-//     // );
-//     next();
-//   });
-//   // it("'Parser#parse'", (next) => {
-//   //   expect(
-//   //     Parser.parse(Parser.unit(1))("abc")
-//   //   ).to.eql(
-//   //     1
-//   //   );
-//   //   next();
-//   // });
-  
-// });
+describe('PretterPrinter', () => {
+  it("print", (next) => {
+    var pair = Pair.cons(1,2);
+    expect(
+      PP.print(pair)
+    ).to.eql(
+      "(1,2)"
+    );
+    next();
+  });
+});

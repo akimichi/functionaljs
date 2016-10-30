a0478b44e381de825318d69317d92af5c05a39f3
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2016-10-30 18:54:40 +0900

    Parser#flatMapを実装

--- test/parser.spec.js
+++ test/parser.spec.js
@@ -9,7 +9,16 @@ var PP = require('../lib/pprinter.js');
 var Parser = require('../lib/parser.js');
 
 describe('パーサーコンビネーター', () => {
+  var abc = String.toList("abc");
   describe("parse", (next) => {
+    it("pure", (next) => {
+      expect(
+        PP.print(Parser.parse(Parser.pure(1))(abc))
+      ).to.eql(
+        '[(1,[a,b,c,nil]),nil]'
+      );
+      next();
+    });
     it("item", (next) => {
       expect(
         PP.print(Parser.item(List.empty()))
@@ -37,6 +46,27 @@ describe('パーサーコンビネーター', () => {
       ).to.eql(
         '[(A,[b,c,nil]),nil]'
       );
+      next();
+    });
+  });
+  describe("monad", (next) => {
+    it("three", (next) => {
+      var input = String.toList("abcdef");
+      var three = Parser.flatMap(Parser.item)((x) => {
+        return Parser.flatMap(Parser.item)((_) => {
+          return Parser.flatMap(Parser.item)((z) => {
+            return Parser.pure(Pair.cons(x,z));
+          });
+        });
+      });
+      expect(
+        PP.print(
+          three(input)
+        )
+      ).to.eql(
+        '[((a,c),[d,e,f,nil]),nil]'
+      );
+
       next();
     });
   });

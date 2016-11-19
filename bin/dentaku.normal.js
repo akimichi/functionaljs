7e15520da632a50c37e4bd60f0415ee442bb5311
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2016-11-20 07:20:23 +0900

    bin/dentaku.normal start working

--- bin/dentaku.normal.js
+++ bin/dentaku.normal.js
@@ -1,14 +1,25 @@
 "use strict";
 
+var Dentaku = require('../lib/dentaku.normal.js');
 var readline = require('readline');
+var prompt = "input> ";
+
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-rl.question("何か入力してください ", function(answer) {
-  // TODO: Log the answer in a database
-  console.log("あなたの入力: ", answer);
-  
-  rl.close();
+rl.on('line', (input) => {
+    console.log(Dentaku.evaluate(input));
+    rl.setPrompt(prompt, prompt.length);
+    rl.prompt();
 });
+
+rl.setPrompt(prompt, prompt.length);
+rl.prompt();
+
+// rl.question("dentaku> ", (input) => {
+//   // TODO: Log the answer in a database
+//   console.log(Dentaku.evaluate(input));
+//   rl.close();
+// });
